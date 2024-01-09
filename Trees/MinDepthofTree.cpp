337542af51c3d29264c7f66f421cfcc77b97fbb6
Dharaneedharan G K <139697363+imdharanee@users.noreply.github.com>
2024-01-09 19:34:14 +0530

    Add files via upload

--- Trees/MinDepthofTree.cpp
+++ Trees/MinDepthofTree.cpp
@@ -1,7 +1,7 @@
-int solve(TreeNode* root,int dist)
+int mindepth(TreeNode* root,int dist)
 {
     if(!root) return INT_MAX;
         if(!root->left and !root->right)
            return dist;
-        return min(height(root->left,dist+1),height(root->right,dist+1));
+        return min(mindepth(root->left,dist+1),mindepth(root->right,dist+1));
 }
